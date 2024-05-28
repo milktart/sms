1ed0739bf4d3e1a34ca80bc3b15a521d4d155e38
milktart <64147590+milktart@users.noreply.github.com>
2024-05-28 12:29:18 -0500

    Update client.js

--- public/client.js
+++ public/client.js
@@ -8,18 +8,19 @@ $(function() {
       $('<li></li>').text(user[0] + " " + user[1]).appendTo('ul#users');
     });
   });
-
+  */
 
   $('form').submit(function(event) {
     event.preventDefault();
     var username = $('input#username').val();
     var password = $('input#password').val();
-    $.post('/login?' + $.param({username:username, password:password}), function() {
+    //$.post('/login?' + $.param({username:username, password:password}), function() {
+    $.post('/login', {username:username, password:password}, function() {
       //$('<li></li>').text(fName + " " + lName).appendTo('ul#users');
       $('input#username').val('');
       $('input#password').val('');
       //$('input').focus();
     });
   });
-  */
+  
 });
