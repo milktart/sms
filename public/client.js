82f1b692fc00f413d0dbf6c139345eb83138ca37
milktart <64147590+milktart@users.noreply.github.com>
2024-05-28 05:51:19 -0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -2,20 +2,22 @@
 // run by the browser each time your view template is loaded
 
 $(function() {
+/*
   $.get('/users', function(users) {
     users.forEach(function(user) {
       $('<li></li>').text(user[0] + " " + user[1]).appendTo('ul#users');
     });
   });
+*/
 
   $('form').submit(function(event) {
     event.preventDefault();
-    var fName = $('input#fName').val();
-    var lName = $('input#lName').val();
-    $.post('/users?' + $.param({fName:fName, lName:lName}), function() {
-      $('<li></li>').text(fName + " " + lName).appendTo('ul#users');
-      $('input#fName').val('');
-      $('input#lName').val('');
+    var username = $('input#username').val();
+    var password = $('input#password').val();
+    $.post('/login?' + $.param({username:username, password:password}), function() {
+      //$('<li></li>').text(fName + " " + lName).appendTo('ul#users');
+      $('input#username').val('');
+      $('input#password').val('');
       $('input').focus();
     });
   });
